93865efcc77383c56bdc02a245c4decfdeef8212
cyberyurei2000 <61709133+cyberyurei2000@users.noreply.github.com>
2024-08-06 01:44:45 -0300

    Add support for config files

--- README.md
+++ README.md
@@ -17,41 +17,44 @@ Simply place the file `/scripts/osc.clock.lua` inside the mpv's script directory
 
 ## Customization
 
-You can customize any aspect of the clock. To do this, go to the beginning of the script to change the settings.
+You can customize any aspect of the clock. To do this, create a `osc-clock.conf` file and place inside the `script-opts` directory in the mpv's configuration directory.
+
+- **Windows**: `%APPDATA%/mpv/script-opts`
+- **Linux**: `~/.config/mpv/script-opts`
+
+### Example
+
+Here is an example of a configuration file, which would be placed in `script-opts/osc-clock.conf`:
+
+```ini
+font=VAG Rounded Std
+fontsize=95
+fontcolor=FFFFFF
+bordersize=3
+bordercolor=000000
+shadowdist=5
+shadowcolor=000000
+blur=0
+```
 
 ![Screenshot1](/docs/screenshot2.png)
 
-```lua
-local config = {
-    font        = "FO-ANN-GMorning2020",
-    fontsize    = 60,
-    fontcolor   = "FFFFFF",
-    bordersize  = 3,
-    bordercolor = "000000",
-    shadowdist  = 0,
-    shadowcolor = "000000",
-    blur        = 2,
-    positionx   = 70,
-    positiony   = 50,
-    format      = "%H:%M",
-    key         = "C"
-}
+### Configuration Options
 
-...
-```
-
-- `font`: Set the font name;
-- `fontsize`: Set the font size;
-- `fontcolor`: Set the font color (format: BBGGRR);
-- `bordersize`: Set the border size, set to 0 to disable the border;
-- `bordercolor`: Set the border color (format: BBGGRR);
-- `shadowdist`: Set the distace of the shadow, set to 0 to disable the shadow;
-- `shadowcolor`: Set the shadow color (format: BBGGRR);
-- `blur`: Set the strength of the blur to apply in the edges of the text, set to 0 to disable it;
-- `positionx`: Set the X position of the clock on screen;
-- `positiony`: Set the Y position of the clock on screen;
-- `format`: Set the time format;
-- `key`: Set the keybinding to toggle the clock;
+|     Options     |           Description            |
+|-----------------|----------------------------------|
+|  `font`         |  Set the font name.
+|  `fontsize`     |  Set the font size.
+|  `fontcolor`    |  Set the font color (format: BBGGRR).
+|  `bordersize`   |  Set the border size, set to 0 to disable the border.
+|  `bordercolor`  |  Set the border color (format: BBGGRR).
+|  `shadowdist`   |  Set the distace of the shadow, set to 0 to disable the shadow.
+|  `shadowcolor`  |  Set the shadow color (format: BBGGRR).
+|  `blur`         |  Set the strength of the blur to apply in the edges of the text, set to 0 to disable it.
+|  `positionx`    |  Set the X position of the clock on screen.
+|  `positiony`    |  Set the Y position of the clock on screen.
+|  `format`       |  Set the time format (format: %H = hour, %M = minutes, %S = seconds).
+|  `key`          |  Set the keybinding to toggle the clock.
 
 ## Credits
 
--- scripts/osc-clock.lua
+++ scripts/osc-clock.lua
@@ -2,7 +2,9 @@
 -- Released under the BSD 3-Clause License
 -- https://opensource.org/license/bsd-3-clause
 
-local config = {
+-- parameters: default user option values
+-- change them using osc-clock.conf in the script-opts directory
+local user_opts = {
     font        = "FO-ANN-GMorning2020",  -- Set the font name
     fontsize    = 60,                     -- Set the font size
     fontcolor   = "FFFFFF",               -- Set the font color (format: BBGGRR)
@@ -13,23 +15,24 @@ local config = {
     blur        = 2,                      -- Set the strength of the blur to apply in the edges of the text, set to 0 to disable it
     positionx   = 70,                     -- Set the X position of the clock on screen
     positiony   = 50,                     -- Set the Y position of the clock on screen
-    format      = "%H:%M",                -- Set the time format
+    format      = "%H:%M",                -- Set the time format (format: %H = hour, %M = minutes, %S = seconds)
     key         = "C"                     -- Set the key to toggle the clock
 }
 
+(require 'mp.options').read_options(user_opts, "osc-clock")
 local osc_clock = mp.create_osd_overlay("ass-events")
 local timer = nil
 local is_shown = false
 
-local font = string.format("{\\fn%s}", config.font)
-local fontsize = string.format("{\\fs%d}", config.fontsize)
-local fontcolor = string.format("{\\1c%s}", config.fontcolor)
-local bordersize = string.format("{\\bord%d}", config.bordersize)
-local bordercolor = string.format("{\\3c%s}", config.bordercolor)
-local shadowdist = string.format("{\\shad%d}", config.shadowdist)
-local shadowcolor = string.format("{\\4c&H%d&}", config.shadowcolor)
-local blur = string.format("{\\blur2}", config.blur)
-local position = string.format("{\\pos(%d,%d)}", config.positionx, config.positiony)
+local font = string.format("{\\fn%s}", user_opts.font)
+local fontsize = string.format("{\\fs%d}", user_opts.fontsize)
+local fontcolor = string.format("{\\1c%s}", user_opts.fontcolor)
+local bordersize = string.format("{\\bord%d}", user_opts.bordersize)
+local bordercolor = string.format("{\\3c%s}", user_opts.bordercolor)
+local shadowdist = string.format("{\\shad%d}", user_opts.shadowdist)
+local shadowcolor = string.format("{\\4c&H%d&}", user_opts.shadowcolor)
+local blur = string.format("{\\blur2}", user_opts.blur)
+local position = string.format("{\\pos(%d,%d)}", user_opts.positionx, user_opts.positiony)
 
 local data = string.format(
     "%s%s%s%s%s%s%s%s%s",
@@ -40,7 +43,7 @@ local data = string.format(
 )
 
 local function clock()
-    local time = os.date(config.format)
+    local time = os.date(user_opts.format)
 
     osc_clock.data = string.format("{\\an7}{\\b1}{\\blur2}%s%s", data, time)
     osc_clock:update()
@@ -61,5 +64,5 @@ local function clock_toggle()
     end
 end
 
-mp.add_key_binding(config.key, "show-clock", clock_toggle)
-mp.msg.verbose(string.format("key: \"%s\" bound to \"show-clock\"", config.key))
+mp.add_key_binding(user_opts.key, "show-clock", clock_toggle)
+mp.msg.verbose(string.format("key: \"%s\" bound to \"show-clock\"", user_opts.key))
